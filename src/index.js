5bc51010637f48d2d276c2d65f6ce2ae82e07056
mu-riaz <74741105+mu-riaz@users.noreply.github.com>
2021-08-13 14:05:52 -0400

    Updated Index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
 
 import App from "./App";
-import Board from "./Board";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <div className="game">
       <div className="game-board">
-        <Board />
+        <App />
       </div>
       <div className="game-info">
         <div>{/* status */}</div>
